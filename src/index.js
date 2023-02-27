cfbff15d682997423a7e63ee29cd35b93aacb240
Misaktig <117006814+Misaktig@users.noreply.github.com>
2023-02-27 22:20:25 +0400

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+        const pairs = new Map(bracketsConfig);
+        const stack = [];
+        for (const char of str) {
+            if (pairs.get(stack.at(-1)) === char) {
+                stack.pop();
+            } else {
+                stack.push(char);
+            }
+        }
+        return stack.length === 0;
 }
